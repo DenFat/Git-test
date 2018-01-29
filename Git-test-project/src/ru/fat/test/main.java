4ae1bfde2a3ccd587e983fa3077e56d7c1ff85b5
fatkhi <zmey2257@mail.ru>
2018-01-29 15:24:36 +0300

    add 1 !

--- Git-test-project/src/ru/fat/test/main.java
+++ Git-test-project/src/ru/fat/test/main.java
@@ -3,7 +3,7 @@ package ru.fat.test;
 public class main {
 
 	public static void main(String[] args) {
-          System.out.println("Hello from Git Hub");
+          System.out.println("Hello from yyy Git Hub");
 	}
 
 }
